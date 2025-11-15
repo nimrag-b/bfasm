26f07417b05a9d4914b9c98cf5eb64fb056ff7aa
nimrag-b <124409677+nimrag-b@users.noreply.github.com>
2025-11-15 12:56:13 +0000

    replace bfasm.c with updated version

--- bfasm.c
+++ bfasm.c
@@ -10,37 +10,46 @@ char mem[MEM_CELLS];
 #define BRACKET_STACK_SIZE 256
 
 
-int main(int argc, char **argv){
-    if(argc == 1){
-        printf("please input a file\n");
-        return -1;
+void strip_ext(char *fname)
+{
+    char *end = fname + strlen(fname);
+
+    while (end > fname && *end != '.') {
+        --end;
     }
 
-    FILE *f = fopen(argv[1], "rb");
-    fseek(f, 0, SEEK_END);
-    long fsize = ftell(f);
-    fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
-
-    char *string = malloc(fsize + 1);
-    fread(string, fsize, 1, f);
-    fclose(f);
-
-    string[fsize] = 0;
-
-    printf("read:\n%s\n\n",string);
-
-    compile(fsize,string,argv[1]);
-    printf("Compiled\n");
-    free(string);
-    return 0;
+    if (end > fname) {
+        *end = '\0';
+    }
 }
 
 
+int assemble(char *cur_folder, char *filename, char *asm_name, char* exe_name){
+
+    strcpy(exe_name,filename);
+    strcat(exe_name,".exe");
+
+    char cmd[1024];
+    sprintf(cmd,"fasmw17332\\FASM.EXE %s %s",asm_name,exe_name);
+
+    sprintf(cmd,"assemble.bat %s %s",asm_name,exe_name);
+
+    printf("Executing: %s\n",cmd);
+
+    system(cmd);
+
+    return 0;
+}
+
+int compile(char* filename, char* asm_name, int len, char *input){
+
+    
+    strcpy(asm_name,filename);
+    strcat(asm_name,".asm");
 
 
-int compile(int len, char *input, char *name){
-    //rax = current index
-    //rbx = current mem ptr
+    // Create a file
+    FILE *fptr = fopen(asm_name, "w");
 
     int bracket_count = 0;
     int brackets[BRACKET_STACK_SIZE];
@@ -48,16 +57,7 @@ int compile(int len, char *input, char *name){
 
     int label = 0;
 
-    FILE *fptr;
 
-    char buf[256];
-
-    strcpy(buf,name);
-
-    strcat(buf,".asm");
-
-    // Create a file
-    fptr = fopen(buf, "w");
 
 
     fprintf(fptr, "format pe console\n\ninclude 'win32ax.inc'\n\nentry start\n\n"); //init slug
@@ -173,13 +173,25 @@ int compile(int len, char *input, char *name){
             break;
         case '[': //jump past matching ] if cell is zero
 
-            fprintf(fptr,"LF%d:\n",bracket_count);
+            int fb = 1;
+            while (input[++index] == '[')
+            {
+                fb++;
+            }
+            index--;
+
             fprintf(fptr,"\tmov al, byte [Memory + ebx]\n");
             fprintf(fptr,"\ttest al, al\n");
-
             fprintf(fptr,"\tjz LB%d\n",bracket_count);
 
-            brackets[bracket_ptr++] = bracket_count++; //inc bracket counter and add to stack
+            fprintf(fptr,"LF%d:\n",bracket_count);
+
+            
+            for (size_t i = 0; i < fb; i++)
+            {
+                brackets[bracket_ptr++] = bracket_count; //inc bracket counter and add to stack
+            }
+            bracket_count++;
             
             break;
         case ']': //jump back to matching [ if cell is not zero
@@ -188,7 +200,10 @@ int compile(int len, char *input, char *name){
             fprintf(fptr,"\ttest al, al\n");
 
             fprintf(fptr,"\tjnz LF%d\n",brackets[--bracket_ptr]);
-            fprintf(fptr,"LB%d:\n",brackets[bracket_ptr]);
+            if(bracket_ptr == 0 || brackets[bracket_ptr] != brackets[bracket_ptr - 1]){
+                fprintf(fptr,"LB%d:\n",brackets[bracket_ptr]);
+            }
+            
 
             break;
         }
@@ -218,5 +233,46 @@ import msvcrt,\\ \n\
 
     // Close the file
     fclose(fptr); 
+    return 0;
+}
+
+
+
+int main(int argc, char **argv){
+    if(argc == 1){
+        printf("please input a file\n");
+        return -1;
+    }
+
+    char name[256];
+    strcpy(name,argv[1]);
+    strip_ext(name);
+
+    FILE *f = fopen(argv[1], "rb");
+    fseek(f, 0, SEEK_END);
+    long fsize = ftell(f);
+    fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
+
+    char *string = malloc(fsize + 1);
+    fread(string, fsize, 1, f);
+    fclose(f);
+
+    string[fsize] = 0;
+
+    printf("read:\n%s\n\n",string);
+
+    char asm_name[256];
+    compile(name,asm_name,fsize,string);
+
+    printf("Compiled\n");
+
+
+    char exe_name[256];
+    assemble(argv[0], name,asm_name, exe_name);
+
+    printf("Assembled into %s\n",exe_name);
+
+
+    free(string);
     return 0;
 }
